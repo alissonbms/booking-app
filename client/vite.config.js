7a66a40b3bcc35dd6dbd87a65b8f9f8bdeb6e2bc
Alisson da Silva <alissonnewdev@gmail.com>
2023-01-09 10:13:07 -0300

    config proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3003",
+        target: "https://booking-app-api-sigma.vercel.app",
         changeOrigin: true,
         secure: false,
       },
